6f15550bf7cde88d114614f1151275cfdf892faf
Lucas <thedeathbatbr@gmail.com>
2023-09-12 12:38:27 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Recreio da Juventude Teste',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
